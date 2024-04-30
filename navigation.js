e9c767c910e9283e394afaf6c995504167d8f158
Dawid Eret <dawidaeret@gmail.com>
2024-04-30 15:58:29 +0100

    added mobile hamburger

--- navigation.js
+++ navigation.js
@@ -18,6 +18,27 @@ const navDivAbout = document.getElementById('navDivAbout');
 const navAbout = document.getElementById('navAbout');
 navAbout.addEventListener('click', () => { navPress(navDivAbout); });
 
+const hambNavList = document.getElementsByClassName('hambNavList');
+const hamburgerButton = document.getElementById("navDivHamburger");
+hamburgerButton.addEventListener('click', hamburgerPress);
+var navSwitch = true;
+hambNavList[0].style.display = 'none';
+hambNavList[1].style.display = 'none';
+hambNavList[2].style.display = 'none';
+hambNavList[3].style.display = 'none';
+hambNavList[4].style.display = 'none';
+
+const navDivTopM = document.getElementById('navDivTopM');
+navDivTopM.addEventListener('click', hamburgerPress);
+const navDivProjectsM = document.getElementById('navDivProjectsM');
+navDivProjectsM.addEventListener('click', hamburgerPress);
+const navDivEducationM = document.getElementById('navDivEducationM');
+navDivEducationM.addEventListener('click', hamburgerPress);
+const navDivExperienceM = document.getElementById('navDivExperienceM');
+navDivExperienceM.addEventListener('click', hamburgerPress);
+const navDivAboutM = document.getElementById('navDivAboutM');
+navDivAboutM.addEventListener('click', hamburgerPress);
+
 function navPress(navDiv) {
     var selected = document.getElementsByClassName("selected");
 
@@ -26,4 +47,22 @@ function navPress(navDiv) {
     }
 
     navDiv.classList.add("selected");
+}
+
+function hamburgerPress() {
+    if (navSwitch) {
+        hambNavList[0].style.display = 'unset';
+        hambNavList[1].style.display = 'unset';
+        hambNavList[2].style.display = 'unset';
+        hambNavList[3].style.display = 'unset';
+        hambNavList[4].style.display = 'unset';
+        navSwitch = false;
+    } else {
+        hambNavList[0].style.display = 'none';
+        hambNavList[1].style.display = 'none';
+        hambNavList[2].style.display = 'none';
+        hambNavList[3].style.display = 'none';
+        hambNavList[4].style.display = 'none';
+        navSwitch = true;
+    }
 }
