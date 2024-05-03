674256e7bf57fed03b73e8301035a50f686b7392
Dawid Eret <dawidaeret@gmail.com>
2024-05-03 21:52:17 +0100

    literally re-done the entire website from scratch for more organised and logical html, css, and javascript. also added confetti

--- navigation.js
+++ navigation.js
@@ -1,68 +1,34 @@
-const navDivTop = document.getElementById('navDivTop');
-const navTop = document.getElementById('navTop');
-navTop.addEventListener('click', () => { navPress(navDivTop); });
+const navbar = document.getElementById("navbar");
 
-const navDivProjects = document.getElementById('navDivProjects');
-const navProjects = document.getElementById('navProjects');
-navProjects.addEventListener('click', () => { navPress(navDivProjects); });
-
-const navDivEducation = document.getElementById('navDivEducation');
-const navEducation = document.getElementById('navEducation');
-navEducation.addEventListener('click', () => { navPress(navDivEducation); });
-
-const navDivExperience = document.getElementById('navDivExperience');
-const navExperience = document.getElementById('navExperience');
-navExperience.addEventListener('click', () => { navPress(navDivExperience); });
-
-const navDivAbout = document.getElementById('navDivAbout');
-const navAbout = document.getElementById('navAbout');
-navAbout.addEventListener('click', () => { navPress(navDivAbout); });
-
-const hambNavList = document.getElementsByClassName('hambNavList');
-const hamburgerButton = document.getElementById("navDivHamburger");
-hamburgerButton.addEventListener('click', hamburgerPress);
-var navSwitch = true;
-hambNavList[0].style.display = 'none';
-hambNavList[1].style.display = 'none';
-hambNavList[2].style.display = 'none';
-hambNavList[3].style.display = 'none';
-hambNavList[4].style.display = 'none';
-
-const navDivTopM = document.getElementById('navDivTopM');
-navDivTopM.addEventListener('click', hamburgerPress);
-const navDivProjectsM = document.getElementById('navDivProjectsM');
-navDivProjectsM.addEventListener('click', hamburgerPress);
-const navDivEducationM = document.getElementById('navDivEducationM');
-navDivEducationM.addEventListener('click', hamburgerPress);
-const navDivExperienceM = document.getElementById('navDivExperienceM');
-navDivExperienceM.addEventListener('click', hamburgerPress);
-const navDivAboutM = document.getElementById('navDivAboutM');
-navDivAboutM.addEventListener('click', hamburgerPress);
-
-function navPress(navDiv) {
-    var selected = document.getElementsByClassName("selected");
-
-    if (selected.length > 0) {
-        selected[0].classList.remove("selected");
-    }
-
-    navDiv.classList.add("selected");
+function navButtonAppend(name, href) {
+    const nav = document.createElement("a");
+    nav.textContent = name;
+    nav.href = href;
+    nav.classList.add("nav-button");
+    nav.addEventListener("click", shootConfetti);
+    navbar.appendChild(nav);
 }
 
-function hamburgerPress() {
-    if (navSwitch) {
-        hambNavList[0].style.display = 'unset';
-        hambNavList[1].style.display = 'unset';
-        hambNavList[2].style.display = 'unset';
-        hambNavList[3].style.display = 'unset';
-        hambNavList[4].style.display = 'unset';
-        navSwitch = false;
-    } else {
-        hambNavList[0].style.display = 'none';
-        hambNavList[1].style.display = 'none';
-        hambNavList[2].style.display = 'none';
-        hambNavList[3].style.display = 'none';
-        hambNavList[4].style.display = 'none';
-        navSwitch = true;
-    }
+function navbarInitialise() {
+    navButtonAppend("TOP", "#image-top");
+    navButtonAppend("PROJECTS", "#div-projects");
+}
+
+navbarInitialise();
+
+function shootConfetti() {
+    confetti({
+        angle: 270,
+        origin: {
+            x: 0.5,
+            y: -1
+        },
+        spread: 270,
+        particleCount: 200,
+        startVelocity: 150,
+        flat: true,
+        zIndex: 9,
+        colors: ["#FFFFFF"],
+        ticks: 100
+    });
 }
